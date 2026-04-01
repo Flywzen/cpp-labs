6d27e47865db1a707107e149506160fd3bddce82
Rafly <fthelittleprince@gmail.com>
2026-04-01 23:06:02 +0700

    Update Overlove-oad.cpp

--- playground/Overlove-oad.cpp
+++ playground/Overlove-oad.cpp
@@ -8,7 +8,7 @@ int main() {
     cout << "System Booting...\n";
     this_thread::sleep_for(chrono::seconds(1));
 
-    // Animasi Loading
+   
     for(int i = 1; i <= 100; i++) {
         cout << "\r\033[36mLoading feelings: [";
         for(int j = 0; j < 20; j++) {
@@ -17,12 +17,12 @@ int main() {
         }
         cout << "] " << i << "%\033[0m" << flush;
         
-        // Bikin loadingnya agak lambat di akhir biar dramatis
+       
         if (i > 80) this_thread::sleep_for(chrono::milliseconds(150));
         else this_thread::sleep_for(chrono::milliseconds(40));
     }
 
-    // Plot Twist
+
     this_thread::sleep_for(chrono::seconds(1));
     cout << "\n\n\033[1;31m[FATAL ERROR] Variable 'Love' exceeded maximum capacity (Integer Overflow).\033[0m\n";
     
